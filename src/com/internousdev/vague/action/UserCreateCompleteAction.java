0e01cc0730d53c74b7aadcaa2d4fab01b60d48b2
aa <aaa>
2018-05-11 14:24:23 +0900

    userCreateComppleteAction , DAO(柳澤）

--- src/com/internousdev/vague/action/UserCreateCompleteAction.java
+++ src/com/internousdev/vague/action/UserCreateCompleteAction.java
@@ -1,17 +1,33 @@
 package com.internousdev.vague.action;
 
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.vague.dao.UserCreateDAO;
+import com.internousdev.vague.dto.LoginUserDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class UserCreateCompleteAction extends ActionSupport{
+public class UserCreateCompleteAction extends ActionSupport implements SessionAware{
+	public Map<String, Object> session;
+	private UserCreateDAO dao = new UserCreateDAO();
 
-	public String execute(){
+	public String execute() throws SQLException {
+		dao.createUserInfo((LoginUserDTO)session.get("CreateUserDTO"));
+
+
+
 
 		String result = SUCCESS;
 
-
 		return result;
 
-
 	}
+@Override
+	public void setSession(Map<String, Object> session){
+	this.session = session;
+}
+
 
 }

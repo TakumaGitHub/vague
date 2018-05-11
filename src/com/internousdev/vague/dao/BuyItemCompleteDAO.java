ab4d9dc0a037a80c9b64b0aa044552f6cb01355b
shunm-999 <shunmiyazaki812@yahoo.co.jp>
2018-05-11 13:56:56 +0900

    更新しました

--- src/com/internousdev/vague/dao/BuyItemCompleteDAO.java
+++ src/com/internousdev/vague/dao/BuyItemCompleteDAO.java
@@ -1,8 +1,34 @@
 package com.internousdev.vague.dao;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.internousdev.vague.dto.AddressDTO;
+import com.internousdev.vague.dto.CartDTO;
+import com.internousdev.vague.util.DBConnector;
+
 /*
  * 決済ボタンを押した後のアクション
  */
 
 public class BuyItemCompleteDAO {
+
+	//カートの中から購入履歴のテーブルに情報を移す
+
+	DBConnector dbConnector = new DBConnector();
+
+	Connection con = (Connection)dbConnector.getConnection();
+
+	List<CartDTO> cartDTOList = new ArrayList<CartDTO>();
+
+	List<AddressDTO> addressDTOList = new ArrayList<AddressDTO>();
+	
+	
+	
+	
+
+
+
+
 }

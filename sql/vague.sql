40f4bb68051583487676c790bab7e0a3b6da2fed
tto8569 <tto8569@192.168.1.111>
2018-05-14 19:08:09 +0900

    sql再度修正。postal_code消しました。

--- sql/vague.sql
+++ sql/vague.sql
@@ -103,7 +103,6 @@ create table destination_info(
 	first_name_kana varchar (32) not null,
 	email varchar (32) not null,
 	tel_number varchar (13) not null,
-	postal_code varchar(11) not null,
 	user_address varchar (50) not null,
 	regist_date datetime not null,
 	update_date datetime
